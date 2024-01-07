f7ab6c30998db45e02fcd3c5307529fe3ae8edff
Hamdi Louati <louati.hamd@gmail.com>
2024-01-07 03:56:32 +0100

    update ci-cd

--- apps/pxm/src/app/app.service.ts
+++ apps/pxm/src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return { message: 'Hello PXM 30' };
+    return { message: 'Hello PXM 40' };
   }
 }
